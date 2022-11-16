f9e8b3eba4bd60561bab576b5b9ece7f1add81e4
saintlyvermin <67609621+saintlyvermin@users.noreply.github.com>
2022-11-16 00:44:53 -0500

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, Jenkins McGill!"); 
+        System.out.println("Hello, Jenkins pipeline McGill!"); 
     }
 }
